b330dd74eca5ffd968f4438e1602144e3347af94
Dan <dmennella34@gmail.com>
2020-05-07 13:47:19 -0400

    added loading spinner

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { popFormat } from '../utils/helper';
 import { Link } from 'react-router-dom';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Loader from 'react-loader-spinner';
 
 import Img from 'react-image';
 const CountryCard = ({ country }) => {
@@ -19,7 +21,16 @@ const CountryCard = ({ country }) => {
         whileHover={{ scale: 1.075 }}
         className="rounded shadow bg-headerbg  "
       >
-        <Img src={flag} className="rounded-t" alt="" />
+        <Img
+          src={flag}
+          loader={
+            <div className="w-full flex justify-center p-5">
+              <Loader type="Oval" color="#00BFFF" height={80} width={80} />
+            </div>
+          }
+          className="rounded-t"
+          alt=""
+        />
         <div className="px-6 py-8 text-headercolor  ">
           <h4 className="font-body text-headercolor   text-lg font-extrabold pb-4">
             {name}
